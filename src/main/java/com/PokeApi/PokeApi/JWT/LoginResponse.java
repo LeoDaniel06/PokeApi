d00e23c7da568956815eb95af7c2aeb79d461a04
digis <digis@ALIEN18>
2026-01-02 16:01:02 -0600

    Agregando el Modulo de Security para autenticacion de usuario, ademas de la generacion y validacion del Token JWT

---
+++ src/main/java/com/PokeApi/PokeApi/JWT/LoginResponse.java
@@ -0,0 +1,40 @@
+
+package com.PokeApi.PokeApi.JWT;
+
+import java.util.Date;
+
+
+public class LoginResponse {
+
+    private final String token;
+    private final Date ExpiresAt;
+    private final Date CreateAt;
+    private String rol;
+    
+    public LoginResponse(String token, Date ExpiresAt, Date CreateAt, String rol){
+        this.token = token;
+        this.ExpiresAt = ExpiresAt;
+        this.CreateAt = CreateAt;
+        this.rol = rol;
+    }
+            
+    public String getToken(){
+        return token;
+    }
+    
+    public Date getCreateAt(){
+        return CreateAt;
+    }
+    
+    public Date getExpiresAt(){
+        return ExpiresAt;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
